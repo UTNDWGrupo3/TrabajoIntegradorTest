ac6f26a8850525017ba2cf699c6cfa986cfcb221
UTNDWGrupo3 <utngrupotres@gmail.com>
2023-04-07 03:07:39 -0300

    Modificaciones varias
    Se subio modificacion de estilos, se subio formulario de contacto, y validacion de campos de formulario

--- js/script.js
+++ js/script.js
@@ -1,2 +1,53 @@
+// declaro variables 
 
+
+// manejo datos de formulario de contacto 
+const frmContato = document.getElementById("frmContacto");
+
+frmContato.addEventListener("submit", function(event)  {
+    // evita que se pierdan los datos del formulario al presionar el boton enviar 
+    event.preventDefault();
+
+    if (validoFrmContacto() === "OK")
+    {
+        // envio datos al pdf 
+    }
+
+} 
+);
+
+// valido que los campos obligatorios se encuentre cargados
+function validoFrmContacto() {
+
+    let resultado="OK";
+
+    if (frmContato.nombre.value == "") {
+
+        alert("Complete el campo nombre");
+        resultado = "ERR";
+        
+    } else  if (frmContato.apellido.value == "") {
+
+        alert("Complete el campo apellido");
+        resultado = "ERR";
+
+    } else if (frmContato.edad.value == "") {
+
+        alert("Complete el campo edad");
+        resultado = "ERR";
+    } else  if (frmContato.email.value == "") {
+
+        alert("Complete el campo email");
+        resultado = "ERR";
+    } else if (frmContato.comentario.value == "") {
+
+        alert("Complete el campo comentario");
+        resultado = "ERR";
+    };
+
+
+    return resultado;
+
+
+}
 
